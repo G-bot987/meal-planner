c90f296b019456af959f27b6140445c99f8be354
gbot987 <ghobs987@gmail.com>
2024-08-16 18:48:57 +0100

    get yesterday and get tomorrow

--- utils/getDay.ts
+++ utils/getDay.ts
@@ -1,4 +1,4 @@
-import { format } from 'date-fns';
+import {addDays, subDays, format } from 'date-fns';
 
 export function getCurrentDay(): {date:string, day:string} {
   const today = new Date();
@@ -8,3 +8,23 @@ export function getCurrentDay(): {date:string, day:string} {
  };
   return formattedDay;
 }
+
+
+export function getTomorrowDay(): {date:string, day:string} {
+    const tomorrow = addDays(new Date(), 1);
+    const formattedDay = {
+    date:  format( tomorrow, 'yyyy-MM-dd'),
+    day: format(tomorrow, 'eeee')
+   };
+    return formattedDay;
+  }
+
+
+  export function getTYesterdayDay(): {date:string, day:string} {
+    const yesterday = subDays(new Date(), 1);
+    const formattedDay = {
+    date:  format( yesterday, 'yyyy-MM-dd'),
+    day: format(yesterday, 'eeee')
+   };
+    return formattedDay;
+  }
