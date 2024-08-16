f976ad3bfd4d27d5d9e9e776a1b3530da12e4b1d
gbot987 <ghobs987@gmail.com>
2024-08-16 19:04:03 +0100

    getting another day func

--- components/diary/MobileDiary.tsx
+++ components/diary/MobileDiary.tsx
@@ -1,43 +1,77 @@
 "use client";
+
 import {
   getCurrentDay,
   getTYesterdayDay,
   getTomorrowDay,
 } from "@/utils/getDay";
+
+import styles from "./diary.module.scss";
+
 import { useEffect, useState } from "react";
 
 export default function MobileDiary() {
   const currentDay = getCurrentDay();
   const [day, setDay] = useState(currentDay);
-  const [getAnotherDay, setGetAnotherDay] = useState(null || String);
+  const [getAnotherDay, setGetAnotherDay] = useState(0);
   useEffect(() => {
-    if (getAnotherDay === "tomorrow") {
-      setDay(getTomorrowDay());
+    if (getAnotherDay > 0) {
+      setDay(getTomorrowDay(getAnotherDay));
     }
 
-    if (getAnotherDay === "yesterday") {
-      setDay(getTYesterdayDay());
+    if (getAnotherDay < 0) {
+      setDay(getTYesterdayDay(getAnotherDay));
     }
   }, [getAnotherDay]);
   return (
-    <div>
-      MobileDiary
-      {day.date}
-      {day.day}
+    <section className={styles.mobile__wrapper}>
       <button
         onClick={() => {
-          setGetAnotherDay("tomorrow");
-        }}
-      >
-        get tomorrow
-      </button>
-      <button
-        onClick={() => {
-          setGetAnotherDay("yesterday");
+          setGetAnotherDay(getAnotherDay - 1);
         }}
       >
         get yesterday
       </button>
-    </div>
+      <table className={styles.table__wrapper__table}>
+        <tbody>
+          <tr className={styles.table__wrapper__table__top__row}>
+            <th
+              className={styles.table__wrapper__table__top__row__meal__collumn}
+            >
+              Meal
+            </th>
+            <th
+              className={styles.table__wrapper__table__top__row__day__collumn}
+            >
+              {day.date}
+              {day.day}
+            </th>
+          </tr>
+          <tr>
+            <td>Breakfast</td>
+          </tr>
+          <tr>
+            <td>Lunch</td>
+          </tr>
+          <tr>
+            <td>Post workout</td>
+          </tr>
+          <tr>
+            <td>Dinner</td>
+          </tr>
+          <tr>
+            <td>snacks</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <button
+        onClick={() => {
+          setGetAnotherDay(getAnotherDay + 1);
+        }}
+      >
+        get tomorrow
+      </button>
+    </section>
   );
 }
