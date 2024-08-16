5e5b0ba3916c1077a0a251daf9d4e520c09f068b
gbot987 <ghobs987@gmail.com>
2024-08-16 17:40:01 +0100

    making comp use client using use effect and state to conditionally show different tables for responsive view

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
 import Diary from "../../components/diary/page";
+import MobileDiary from "@/components/diary/MobileDiary";
 
 export default function Dashboard() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    // clean up function will only run in unmounting phase not executed initially
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <div>
       super mega awesome dashbord page
-      <Diary />
+      {isMobile ? <MobileDiary /> : <Diary />}
     </div>
   );
 }
