ba962a4625c8fb30316d3e22eef117834656a6c3
gbot987 <ghobs987@gmail.com>
2024-08-16 18:49:29 +0100

    basic functionality of getting different days and dates for mobile table render

--- components/diary/MobileDiary.tsx
+++ components/diary/MobileDiary.tsx
@@ -1,12 +1,43 @@
-import { getCurrentDay } from "@/utils/getDay";
+"use client";
+import {
+  getCurrentDay,
+  getTYesterdayDay,
+  getTomorrowDay,
+} from "@/utils/getDay";
+import { useEffect, useState } from "react";
 
 export default function MobileDiary() {
   const currentDay = getCurrentDay();
+  const [day, setDay] = useState(currentDay);
+  const [getAnotherDay, setGetAnotherDay] = useState(null || String);
+  useEffect(() => {
+    if (getAnotherDay === "tomorrow") {
+      setDay(getTomorrowDay());
+    }
+
+    if (getAnotherDay === "yesterday") {
+      setDay(getTYesterdayDay());
+    }
+  }, [getAnotherDay]);
   return (
     <div>
       MobileDiary
-      {currentDay.date}
-      {currentDay.day}
+      {day.date}
+      {day.day}
+      <button
+        onClick={() => {
+          setGetAnotherDay("tomorrow");
+        }}
+      >
+        get tomorrow
+      </button>
+      <button
+        onClick={() => {
+          setGetAnotherDay("yesterday");
+        }}
+      >
+        get yesterday
+      </button>
     </div>
   );
 }
