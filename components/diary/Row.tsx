f979c4fe4dd3f720c7459f0ce77520360ff08536
gbot987 <ghobs987@gmail.com>
2024-08-20 17:41:04 +0100

    row conditional classes nth child didnt work;

--- components/diary/Row.tsx
+++ components/diary/Row.tsx
@@ -1,17 +1,24 @@
-import { tr } from "date-fns/locale";
 import { useState } from "react";
+import styles from "./diary.module.scss";
 
 interface mealInterface {
   meal: string;
+  index: number;
 }
 export default function Row(props: mealInterface) {
-  const { meal } = props;
+  const { meal, index } = props;
   const [logged, setLogged] = useState(null);
   const [showAddMenu, setShowAddmenu] = useState(false);
   return (
     <>
       <tr>
-        <td>{meal}</td>
+        <td
+          className={`${styles.table__wrapper__table__meal__row}  ${
+            index === 4 ? styles["table__wrapper__table__meal__row__last"] : ""
+          }`}
+        >
+          {meal}
+        </td>
         <td>
           <button
             onClick={() => {
