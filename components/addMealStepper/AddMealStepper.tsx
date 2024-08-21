809114a931c3960b257d476ea70d40e4cf9a8190
gbot987 <ghobs987@gmail.com>
2024-08-21 02:20:31 +0100

    add meal stepper ui init

---
+++ components/addMealStepper/AddMealStepper.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./addMealStepper.module.scss";
+
+export default function AddMealStepper() {
+  return (
+    <section className={styles.add__stepper}>
+      <ul>
+        <li>its</li>
+        <li>time </li>
+        <li>to add</li>
+        <li>a meal</li>
+      </ul>
+    </section>
+  );
+}
