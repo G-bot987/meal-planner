359398bb9b133946fa3fd61a443760c9825d3200
gbot987 <ghobs987@gmail.com>
2024-08-23 17:01:43 +0100

    abstracting

--- components/addMealStepper/stepOne/search/searchResults/FoodResults.tsx
+++ components/addMealStepper/stepOne/search/searchResults/FoodResults.tsx
@@ -1,24 +1,9 @@
+import { FOODINTERFACE } from "@/utils/interfaces/mealsAndFoodsInterfaces/interfaces";
 import React from "react";
 
-export interface FOODINTERFACE {
-  id: number;
-  name: string;
-  calories: number;
-  carbohydrates: number;
-  fat: number;
-  fibre: number;
-  protein: number;
-  sugar: number;
-  weight: number;
-  created_at: string;
-  updated_at: string;
-  deleted_at: string | null;
-  user_id: number | null;
-}
 interface PROPSINTERFACE {
   food: FOODINTERFACE;
 }
-
 export default function FoodResults(props: PROPSINTERFACE) {
   const { food } = props;
   const { name } = food;
