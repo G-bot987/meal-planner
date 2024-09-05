ff4a530b7398477f146724da7c24881eb6de2353
gbot987 <ghobs987@gmail.com>
2024-09-05 18:34:34 +0100

    updating routes to match new structure as per foods

---
+++ app/api/meals/[search]/[typeOf]/route.ts
@@ -0,0 +1,12 @@
+
+import { NextResponse } from 'next/server'
+import { searchMeals } from '../../../services/meal';
+
+export async function GET(request: Request, context:any) {
+
+    const {params} = context;
+
+    const data = await searchMeals(params.search)
+
+    return NextResponse.json(data)  
+}
