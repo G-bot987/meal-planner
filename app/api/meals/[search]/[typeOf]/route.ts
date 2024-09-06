fef0e22677e8fff807c60a2907d09fe099c4d7a2
gbot987 <ghobs987@gmail.com>
2024-09-06 14:26:27 +0100

    new meal routing for global and personal

--- app/api/meals/[search]/[typeOf]/route.ts
+++ app/api/meals/[search]/[typeOf]/route.ts
@@ -1,12 +1,31 @@
 
 import { NextResponse } from 'next/server'
-import { searchMeals } from '../../../services/meal';
+import { globalSearchMeals, personalSearchMeals } from '../../../services/meal';
+import { authOptions } from '@/utils/authOptions';
+import { getServerSession } from 'next-auth';
 
 export async function GET(request: Request, context:any) {
 
-    const {params} = context;
+    const {params:{typeOf, search}} = context;
 
-    const data = await searchMeals(params.search)
-
-    return NextResponse.json(data)  
+    const session = await getServerSession(authOptions);
+    const id = session?.user.id
+    
+    try {
+    switch (typeOf) {
+        case 'global':
+            const data = await globalSearchMeals(search)
+            return   NextResponse.json(data)  
+        case 'personal':
+            const personalData = await personalSearchMeals(search, id)
+            return   NextResponse.json(personalData)
+        default:
+            return NextResponse.json({ message: 'Invalid search type' }, { status: 400 });  
+      }
+    } catch (error:any) {
+    console.error('Error in search:', error);
+    return NextResponse.json({ message: 'Something went wrong', error: error.message }, { status: 500 });
+    }
 }
+
+
