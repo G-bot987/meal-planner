2c029e0c427db7cb0ad6333bf754595bac4740c2
gbot987 <ghobs987@gmail.com>
2024-09-07 21:39:11 +0100

    basic template for food name ready to interact with zustland store needs styling

--- components/addMealStepper/stepOne/create/new/food/NewFood.tsx
+++ components/addMealStepper/stepOne/create/new/food/NewFood.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./newFood.module.scss";
 
 export default function NewFood() {
-  return <div>NewFood</div>;
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const userInput = e.target.value;
+    setSearchValue(userInput);
+  };
+  return (
+    <section>
+      <h1>give your food a name</h1>
+
+      <section className={styles.form}>
+        <label htmlFor="foodName">give your food a name </label>
+        <input
+          type="text"
+          value={searchValue}
+          onChange={handleInput}
+          placeholder={`food name`}
+        />
+        <button className={styles.form__submit}> next </button>
+      </section>
+    </section>
+  );
 }
