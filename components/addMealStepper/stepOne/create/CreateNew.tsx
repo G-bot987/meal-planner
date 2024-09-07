2330876e8ea4018d7ae701b5f92c3f14958f516b
gbot987 <ghobs987@gmail.com>
2024-09-07 02:45:40 +0100

    add new food meal basic ux and ui styling for stepper menu

--- components/addMealStepper/stepOne/create/CreateNew.tsx
+++ components/addMealStepper/stepOne/create/CreateNew.tsx
@@ -4,7 +4,18 @@ import styles from "./create.module.scss";
 export default function CreateNew() {
   return (
     <section className={styles.create__wrapper}>
-      <h1>create new food or meal</h1>
+      <h1 className={styles.create__wrapper__header}>
+        are you updating a version of a meal or food{" "}
+      </h1>
+
+      <article className={styles.create__wrapper__btn__wrapper}>
+        <button className={styles.create__wrapper__btn__wrapper__btn}>
+          yes
+        </button>
+        <button className={styles.create__wrapper__btn__wrapper__btn}>
+          no
+        </button>
+      </article>
     </section>
   );
 }
