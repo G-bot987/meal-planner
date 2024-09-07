37db2cbbd09c48e414effc94440bbd3b62fc8936
gbot987 <ghobs987@gmail.com>
2024-09-07 01:48:21 +0100

    create new step comp

---
+++ components/addMealStepper/stepOne/create/CreateNew.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./create.module.scss";
+
+export default function CreateNew() {
+  return (
+    <section className={styles.create__wrapper}>
+      <h1>create new food or meal</h1>
+    </section>
+  );
+}
