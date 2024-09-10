556f8365da288ea6c4090dfef7d2627f3fd494d6
gbot987 <ghobs987@gmail.com>
2024-09-10 03:29:35 +0100

    using new stores

--- components/addMealStepper/stepOne/search/searchResults/MealResults.tsx
+++ components/addMealStepper/stepOne/search/searchResults/MealResults.tsx
@@ -1,5 +1,5 @@
 import { MEALSINTERFACE } from "@/utils/interfaces/mealsAndFoodsInterfaces/interfaces";
-import { mealStepperStore } from "@/zustland/store/store";
+import { createNewStore } from "@/zustland/store/store";
 import React from "react";
 import ArchivedMeals from "./archivedMeals/ArchivedMeals";
 interface PROPSINTERFACE {
@@ -9,7 +9,7 @@ export default function MealResults(props: PROPSINTERFACE) {
   const {
     meal: { name, archived_meals },
   } = props;
-  const { setStepForMealStepper } = mealStepperStore();
+  const { changeStep } = createNewStore();
 
   return (
     <li>
@@ -17,7 +17,7 @@ export default function MealResults(props: PROPSINTERFACE) {
 
       <button
         onClick={() => {
-          setStepForMealStepper(1);
+          changeStep(1);
         }}
       >
         select meal
