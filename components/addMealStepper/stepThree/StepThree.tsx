267740aa6d439103fd72b2aa368f89c7c9fb0061
gbot987 <ghobs987@gmail.com>
2024-09-10 03:29:55 +0100

    step three new location

---
+++ components/addMealStepper/stepThree/StepThree.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "../addMealStepper.module.scss";
+
+export default function StepThree() {
+  return <section className={styles.step__three}>StepThree</section>;
+}
