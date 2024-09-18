2c599db62e27ae573d483709d031751c9416e318
gbot987 <ghobs987@gmail.com>
2024-09-18 02:10:19 +0100

    commenting

--- components/addMealStepper/stepOne/create/new/New.tsx
+++ components/addMealStepper/stepOne/create/new/New.tsx
@@ -4,10 +4,13 @@ import NewFood from "./food/NewFood";
 import { createNewStore } from "@/zustland/store/store";
 export default function New() {
   const [creatingNew, setCreatingNew] = useState<string | null>(null);
-  const { changeStep } = createNewStore();
-  useEffect(() => {
-    changeStep(0);
-  }, []);
+
+  //tried to understand why this is here commented out, i was setting stepper state on load to initially fix bugs but removing enables the loading of this comp from the nutritional screen, when the user isn't the food creator. tried to replicate reason for this and look for bugs when removed couldn't find any so commenting for now
+
+  // const { changeStep } = createNewStore();
+  // useEffect(() => {
+  //   changeStep(0);
+  // }, []);
 
   return (
     <article className={styles.create__wrapper}>
