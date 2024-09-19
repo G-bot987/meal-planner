db600bc922cdb5b016f29d76897a815f2f2edf93
gbot987 <ghobs987@gmail.com>
2024-09-19 21:46:06 +0100

    basic confirmation of nutritional changes screen

---
+++ components/addMealStepper/stepTwo/Confirmation.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { FORMDATA } from "./StepTwo";
+
+export default function Confirmation(props: Partial<FORMDATA>) {
+  console.log("props");
+  console.log(props);
+  console.log("---");
+  return <div>Confirmation</div>;
+}
