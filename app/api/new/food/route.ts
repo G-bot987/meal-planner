65fa4f4d505a69c8d896e2b499957d013f820490
gbot987 <ghobs987@gmail.com>
2024-11-08 03:01:16 +0000

    try catch wrapper for new food api route to throw error to client

--- app/api/new/food/route.ts
+++ app/api/new/food/route.ts
@@ -13,10 +13,14 @@ export async function POST(request: NextRequest) {
         const id = session?.user.id
     
         const res = await addFood(foodToCreate, id)
-    
+
         return   NextResponse.json(res)
         
     } catch (error:any) {
-            return NextResponse.json({ message: 'your food could not be created', error: error.message }, { status: 500 });
+        console.log('catch wrapper')
+        console.log(error.message)
+        console.log(error.cause)
+        console.log('---')
+        return NextResponse.json({ message: 'your food could not be created', error: error.message, cause: error.cause }, { status: error.code });
     }
 }
